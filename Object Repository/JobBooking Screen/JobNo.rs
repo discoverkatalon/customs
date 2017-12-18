bc6f444ac83f38a4e9651cac5178ad6304aeb9f0
discoverkatalon <discover.katalon@gmail.com>
2017-12-18 18:59:12 +0530

    Deleted Unwanted TC

---
+++ Object Repository/JobBooking Screen/JobNo.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>JobNo</name>
+   <tag></tag>
+   <elementGuidId>f10e2f1f-6b62-4828-b071-b171bdf75d85</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*{}</value>
+   </webElementProperties>
+</WebElementEntity>
